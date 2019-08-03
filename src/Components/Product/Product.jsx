aa9c88e2d491e4e63927d54834a11f1d96d36f41
AxYleN <jeka2644@gmail.com>
2019-08-03 17:32:11 +0300

    Рефакторинг компонента Product

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -3,55 +3,52 @@ import './Product.css';
 import SellerInfo from 'Components/SellerInfo/SellerInfo';
 import { favoritesContext } from 'Containers/ProductsDataProvider/ProductsDataProvider';
 
+const formatPrice = price => {
+  const options = {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 0,
+  };
+
+  return price.toLocaleString('ru', options);
+};
+
 export default function Product({ data }) {
-  const sellerData = data.seller;
   const { favorites, addToFavorites, removeFromFavorites } = useContext(favoritesContext);
+  const { id, title, seller, pictures } = data;
 
-  const image = data.pictures[0];
-  const imagesCount = data.pictures.length - 1;
-  const { id, title } = data;
-  const price = data.price
-    ? data.price.toLocaleString('ru', {
-        style: 'currency',
-        currency: 'RUB',
-        minimumFractionDigits: 0,
-      })
-    : null;
-
+  const picturesCount = pictures.length - 1;
+  const price = data.price ? formatPrice(data.price) : 'Цена не указана';
   const isInFavorites = favorites.includes(id);
 
   const handleClick = () => {
     if (isInFavorites) {
       return removeFromFavorites(id);
     }
-    return addToFavorites(id);
+    addToFavorites(id);
   };
+
+  const heartClassNames = ['product__heart-icon'];
+  if (isInFavorites) heartClassNames.push('product__heart-icon--active');
 
   return (
     <div className="product">
       <div className="product__image-group">
-        <img src={image} alt={title} className="product__image" loading="lazy" />
+        <img src={pictures[0]} alt={title} className="product__image" />
         <button className="product__favorites-btn" onClick={handleClick}>
-          <svg
-            width="20"
-            height="20"
-            className={
-              isInFavorites
-                ? 'product__heart-icon product__heart-icon--active'
-                : 'product__heart-icon'
-            }
-            xmlns="http://www.w3.org/2000/svg">
+          <svg width="20" height="20" className={heartClassNames.join(' ')}>
             <use href="icons.svg#icon-heart" />
           </svg>
         </button>
-        <div className="product__images-count" title={`Дополнительных фото: ${imagesCount}`}>
-          {imagesCount === 0 ? null : `+${imagesCount}`}
+        <div className="product__images-count" title={`Дополнительных фото: ${picturesCount}`}>
+          {picturesCount === 0 ? null : `+${picturesCount}`}
         </div>
       </div>
+
       <div className="product__info">
         <h2 className="product__title">{title}</h2>
-        <p className="product__price">{price || 'Цена не указана'}</p>
-        <SellerInfo data={sellerData} />
+        <p className="product__price">{price}</p>
+        <SellerInfo data={seller} />
       </div>
     </div>
   );
