4c910a82b19d30185754d8b7c2c685731e0631ce
AxYleN <jeka2644@gmail.com>
2019-08-03 17:10:35 +0300

    Рефакторинг ProductsDataProvider

--- src/Containers/ProductsDataProvider/ProductsDataProvider.jsx
+++ src/Containers/ProductsDataProvider/ProductsDataProvider.jsx
@@ -3,15 +3,89 @@ import ProductList from 'Containers/ProductList/ProductList';
 
 const favoritesContext = React.createContext();
 
+const getProducts = async () => {
+  const [productsRes, sellersRes] = await Promise.all([
+    fetch('//avito.dump.academy/products').then(res => res.json()),
+    fetch('//avito.dump.academy/sellers').then(res => res.json()),
+  ]);
+
+  const sellers = {};
+  sellersRes.data.forEach(seller => {
+    sellers[seller.id] = seller;
+  });
+
+  return productsRes.data.map(product => {
+    const sellerId = product.relationships.seller;
+    return { ...product, seller: sellers[sellerId] };
+  });
+};
+
+const getFavorites = () => {
+  const favorites = localStorage.getItem('favorites');
+  if (!favorites) return [];
+  return JSON.parse(favorites);
+};
+
+const filterProducts = (products, filter, favorites, favoritesOnly) => {
+  if (favoritesOnly) {
+    products = products.filter(({ id }) => favorites.includes(id));
+  }
+
+  const { category, minPrice, maxPrice } = filter;
+  if (category !== 'any') {
+    products = products.filter(product => product.category === category);
+  }
+
+  if (minPrice !== '') {
+    products = products.filter(({ price }) => price >= minPrice);
+  }
+  if (maxPrice !== '') {
+    products = products.filter(({ price }) => price <= maxPrice);
+  }
+
+  return products;
+};
+
+const sortProducts = (products, orderBy) => {
+  if (orderBy === 'rating') {
+    return products.sort((a, b) => b.seller.rating - a.seller.rating);
+  }
+  if (orderBy === 'price') {
+    return products.sort((a, b) => {
+      if (a.price === undefined) return 1;
+      if (b.price === undefined) return -1;
+      return a.price - b.price;
+    });
+  }
+};
+
 export default function ProductsDataProvider({ filter, favoritesOnly }) {
   const [productList, setProductList] = useState([]);
-  const [productListFavorites, setProductListFavorites] = useState([]);
+  const [productListFavorites, setProductListFavorites] = useState(getFavorites());
+
+  // Загрузка избранных объявлений
+  useEffect(() => {
+    const updateFav = e => {
+      if (e.key === 'favorites') {
+        setProductListFavorites(getFavorites());
+      }
+    };
+
+    window.addEventListener('storage', updateFav);
+    return () => window.removeEventListener('storage', updateFav);
+  }, []);
+
+  // Загрузка списка объявлений
+  useEffect(() => {
+    getProducts().then(setProductList);
+  }, []);
+
+  const products = filterProducts(productList, filter, productListFavorites, favoritesOnly);
 
   const setFavorites = favorites => {
     setProductListFavorites(favorites);
     localStorage.setItem('favorites', JSON.stringify(favorites));
   };
-
   const addToFavorites = id => {
     if (productListFavorites.includes(id)) return;
     setFavorites([...productListFavorites, id]);
@@ -20,77 +94,14 @@ export default function ProductsDataProvider({ filter, favoritesOnly }) {
     setFavorites(productListFavorites.filter(productId => productId !== id));
   };
 
-  useEffect(() => {
-    (async function() {
-      const [productsRes, sellersRes] = await Promise.all([
-        fetch('//avito.dump.academy/products').then(res => res.json()),
-        fetch('//avito.dump.academy/sellers').then(res => res.json()),
-      ]);
-
-      const sellers = {};
-      sellersRes.data.forEach(seller => {
-        sellers[seller.id] = seller;
-      });
-
-      const products = productsRes.data.map(product => {
-        const sellerId = product.relationships.seller;
-        return { ...product, seller: sellers[sellerId] };
-      });
-
-      setProductList(products);
-    })();
-  }, []);
-
-  useEffect(() => {
-    const loadFavorites = () => {
-      const favorites = localStorage.getItem('favorites');
-      if (favorites) {
-        setProductListFavorites(JSON.parse(favorites));
-      }
-    };
-    loadFavorites();
-
-    const updateFavorites = e => {
-      if (e.key === 'favorites') {
-        loadFavorites();
-      }
-    };
-
-    window.addEventListener('storage', updateFavorites);
-
-    return () => {
-      window.removeEventListener('storage', updateFavorites);
-    };
-  }, []);
-
-  let products = productList;
-
-  if (favoritesOnly) {
-    products = products.filter(product => productListFavorites.includes(product.id));
-  }
-
-  if (filter.category !== 'any') {
-    products = products.filter(product => product.category === filter.category);
-  }
-  if (filter.minPrice !== '') {
-    products = products.filter(product => product.price >= filter.minPrice);
-  }
-  if (filter.maxPrice !== '') {
-    products = products.filter(product => product.price <= filter.maxPrice);
-  }
-
-  products = products.sort((a, b) => {
-    if (filter.order === 'rating') return b.seller.rating - a.seller.rating;
-
-    if (a.price === undefined) return 1;
-    if (b.price === undefined) return -1;
-    return a.price - b.price;
-  });
-
+  const favoritesContextValue = {
+    favorites: productListFavorites,
+    addToFavorites,
+    removeFromFavorites,
+  };
   return (
-    <favoritesContext.Provider
-      value={{ favorites: productListFavorites, addToFavorites, removeFromFavorites }}>
-      <ProductList products={products} />
+    <favoritesContext.Provider value={favoritesContextValue}>
+      <ProductList products={sortProducts(products, filter.order)} />
     </favoritesContext.Provider>
   );
 }
