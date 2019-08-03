8c3222baa8381708974c5d902ae6dbe5011ecf29
AxYleN <jeka2644@gmail.com>
2019-08-03 13:29:06 +0300

    Updates

---
+++ precache-manifest.1ba9dbc16b5dbf21885433e423624ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43ec14b26a177fffb1c9d7c206dee72",
+    "url": "/avito/index.html"
+  },
+  {
+    "revision": "4d2a224234e198231b57",
+    "url": "/avito/static/css/main.f09eca1a.chunk.css"
+  },
+  {
+    "revision": "0697fc80fb3e7aa3ed42",
+    "url": "/avito/static/js/2.7ac3102f.chunk.js"
+  },
+  {
+    "revision": "4d2a224234e198231b57",
+    "url": "/avito/static/js/main.7e5f1e67.chunk.js"
+  },
+  {
+    "revision": "f51a4b2a72bafe583956",
+    "url": "/avito/static/js/runtime~main.20751a04.js"
+  }
+]);
